481464ec34ad3b5c367b63c850c8cdf875c35148
Rosenthal <erosenthal@r4xfmk99mt.hq.corp.viasat.com>
2024-02-20 16:23:50 -0500

    fix trace file

--- internal/process_test.py
+++ internal/process_test.py
@@ -586,6 +586,27 @@ class ProcessTest(object):
                     pass
         except Exception:
             logging.exception('Error merging trace page data')
+<<<<<<< Updated upstream
+=======
+            
+    def snappi_merge_trace_page_data(self):
+        """Extract and merge page data from snappi trace events.
+            TODO: looping over all trace events again here, must be a better place for this.
+        """
+        from internal.snappi import snappi_trace_parser
+        try:
+            page_data = self.data['pageData']
+            metrics_file = os.path.join(self.task['dir'], self.prefix + '_trace.json.gz')
+            if os.path.isfile(metrics_file):
+                with gzip.open(metrics_file, GZIP_READ_TEXT) as f:
+                    trace_metrics = json.load(f)
+                    snappi_metrics = snappi_trace_parser.snappi_parse_trace(trace_metrics)
+                    if snappi_metrics:
+                        for key in snappi_metrics:
+                            page_data[key] = snappi_metrics[key]
+        except Exception:
+            logging.exception('Error merging snappi trace page data')
+>>>>>>> Stashed changes
 
     def merge_priority_streams(self):
         """Merge the list of HTTP/2 priority-only stream data"""
